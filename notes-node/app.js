b6d2989a6eb49741313cee4fefe9c41f02a54299
chrisxjacobi <chrisxjacobi@gmail.com>
2018-01-02 16:16:45 -0600

    reviewed process object, process.argv, and the third item in array which can be first address of command line args, used yargs npm package to parse arguments

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,11 +2,31 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command:', command);
+console.log('Yargs:', argv);
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  notes.getNote(argv.title);
+} else if (command === 'remove') {
+  notes.removeNote(argv.title);
+} else {
+  console.log('Command not recognized');
+}
 
 
+
+
+// reviewed process object, process.argv, and the third item in array which can be first address of command line args, used yargs npm package to parse arguments
 
 
 /*
