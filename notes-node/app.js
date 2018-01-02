e2c60b8e460a5b89042bbf68bfd7b9c142b01c3f
chrisxjacobi <chrisxjacobi@gmail.com>
2018-01-02 10:08:39 -0600

    practice npm installs and using 3rd party modules, address package.json file, used lodash for type checking

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,7 +2,23 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
+
+
+
+
+// testing lodash .uniq method
+var filteredArray = _.uniq(['chris', 1, 'chris', 1, 2, 3, 4])
+console.log(filteredArray);
+
+/*
+//testing lodash isString method
+console.log(_.isString(true));
+console.log(_.isString('cxj'));
+
+
 
 var res = notes.addNote();
 console.log(res);
@@ -10,6 +26,7 @@ console.log(res);
 console.log('Result:', notes.add(7, 19));
 
 
-// var user = os.userInfo();
-//
-// fs.appendFileSync('greetings.txt', ` Hello ${user.username}! You are ${notes.age}.`);
+var user = os.userInfo();
+
+fs.appendFileSync('greetings.txt', ` Hello ${user.username}! You are ${notes.age}.`);
+*/
