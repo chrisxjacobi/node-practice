a49fd78e225e7b203e4348cde36fcb0c29943fb2
chrisxjacobi <chrisxjacobi@gmail.com>
2018-01-02 10:19:26 -0600

    installed nodemon globally, ignoring node_modules when pushing to github

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,7 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
@@ -9,24 +8,23 @@ const notes = require('./notes.js');
 
 
 
+
+/*
 // testing lodash .uniq method
 var filteredArray = _.uniq(['chris', 1, 'chris', 1, 2, 3, 4])
 console.log(filteredArray);
 
-/*
-//testing lodash isString method
+// testing lodash isString method
 console.log(_.isString(true));
 console.log(_.isString('cxj'));
 
-
-
+// referencing function in notes.js
 var res = notes.addNote();
 console.log(res);
-
 console.log('Result:', notes.add(7, 19));
 
-
+// testing node medthod os
 var user = os.userInfo();
-
+// testing node method appendFile
 fs.appendFileSync('greetings.txt', ` Hello ${user.username}! You are ${notes.age}.`);
 */
