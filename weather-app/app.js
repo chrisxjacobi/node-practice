1e45077da3c68cd5f7280b9fed1288a4191b0d4d
chrisxjacobi <chrisxjacobi@gmail.com>
2018-01-06 15:18:37 -0600

    used darksky.net API to find real time weather data, tested request with local austin zipcode

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,30 +1,48 @@
-// set yargs options, encode/decode URI Components, set error handling for unavailable address and server errors...exported logic to another directory and used a callback for error handling
-// API key = AIzaSyAVOrV5-E0f-LY20p_aDdnTgn-k8RSolq0
+// used darksky.net API to find real time weather data, tested request with local austin zipcode
 
-const yargs = require('yargs');
+// google maps API key = AIzaSyAVOrV5-E0f-LY20p_aDdnTgn-k8RSolq0
+// forecast.io API = 9f1d9474a178bdf8328ea19d1c596051
+// url: https://api.darksky.net/forecast/9f1d9474a178bdf8328ea19d1c596051/30.3248256,-97.73895429999999
 
-const geocode = require('./geocode/geocode.js');
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode.js');
+//
+// const argv = yargs
+// .options({
+//   a: {
+//     demand: true,
+//     alias: 'address',
+//     describe: 'Address to fetch weather for',
+//     string: true
+//   }
+// })
+// .help()
+// .alias('help', 'h')
+// .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 
-const argv = yargs
-.options({
-  a: {
-    demand: true,
-    alias: 'address',
-    describe: 'Address to fetch weather for',
-    string: true
-  }
-})
-.help()
-.alias('help', 'h')
-.argv;
+//make a call to request to request url, print body.currently.temperature
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
+const request = require('request');
+
+request({
+  url: `https://api.darksky.net/forecast/9f1d9474a178bdf8328ea19d1c596051/30.3248256,-97.73895429999999`,
+  json: true
+}, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect to forecast.io servers.');
+  } else if (response.statusCode === 400) {
+    console.log('Unable to fetch error.');
+  } else if (response.statusCode === 200) {
+    console.log(`Current temperate: ${body.currently.temperature}`);
+
   }
 });
-
-
-// create new directory and file, call geocode.geocodeAddress(argv.a), export geocodeAddress function, move logic to new file, load request into new file and delete on app.js, call first argument 'address';
