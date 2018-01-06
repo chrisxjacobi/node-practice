bbe5cb9ef07ac29a393e117d4e3a4fa901b32241
chrisxjacobi <chrisxjacobi@gmail.com>
2018-01-06 11:30:24 -0600

    pretty printed body (to be displayed on terminal with node) with JSON.stringify, followed response, and tested error logging...also printed some information from the request to the weather API(address, lat, lng)

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
-// practiced callback functions with timeouts for illustration of async, used npm request backage to call a google maps API for an address using generated API key
+// pretty printed body (to be displayed on terminal with node) with JSON.stringify, followed response, and tested error logging...also printed some information from the request to the weather API(address, lat, lng)
+// API key = AIzaSyAVOrV5-E0f-LY20p_aDdnTgn-k8RSolq0
 
 const request = require('request');
 
@@ -6,5 +7,7 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+st+philadelphia&key=AIzaSyAVOrV5-E0f-LY20p_aDdnTgn-k8RSolq0',
   json: true
 }, (error, response, body) => {
-  console.log(body);
-})
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+});
