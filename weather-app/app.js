63bd502b9f863654808305cd9b027fec2df4741b
chrisxjacobi <chrisxjacobi@gmail.com>
2018-01-06 14:57:48 -0600

    set yargs options, encode/decode URI Components, set error handling for unavailable address and server errors...exported logic to another directory and used a callback for error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,30 @@
-// pretty printed body (to be displayed on terminal with node) with JSON.stringify, followed response, and tested error logging...also printed some information from the request to the weather API(address, lat, lng)
+// set yargs options, encode/decode URI Components, set error handling for unavailable address and server errors...exported logic to another directory and used a callback for error handling
 // API key = AIzaSyAVOrV5-E0f-LY20p_aDdnTgn-k8RSolq0
 
-const request = require('request');
+const yargs = require('yargs');
 
-request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+st+philadelphia&key=AIzaSyAVOrV5-E0f-LY20p_aDdnTgn-k8RSolq0',
-  json: true
-}, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+const geocode = require('./geocode/geocode.js');
+
+const argv = yargs
+.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
 });
+
+
+// create new directory and file, call geocode.geocodeAddress(argv.a), export geocodeAddress function, move logic to new file, load request into new file and delete on app.js, call first argument 'address';
