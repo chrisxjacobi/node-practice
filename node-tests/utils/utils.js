fe36d232b8514d432ac875f2540843fd137f0221
chrisxjacobi <chrisxjacobi@gmail.com>
2018-01-10 09:46:29 -0600

    explored how to create a test suite with mocha, created a test case/script, updated package.json to test mocha with npm test

---
+++ node-tests/utils/utils.js
@@ -0,0 +1,7 @@
+// explored how to create a test suite with mocha, created a test case/script, updated package.json to test mocha with npm test
+
+module.exports.add = (a, b) =>  a + b;
+
+module.exports.square = (x) => {
+  return x * x;
+};
---
+++ node-tests/utils/utils.test.js
@@ -0,0 +1,19 @@
+const utils = require('./utils.js');
+
+it('should add two numbers', () => {
+  var res = utils.add(33, 11);
+
+  if (res !== 44) {
+    throw new Error(`Expected 44, but got ${res}.`);
+  }
+
+});
+
+
+it('should square a number', () => {
+  var res = utils.square(9);
+
+  if (res !== 81) {
+    throw new Error(`Expected 81, but got ${res}.`);
+  }
+})
