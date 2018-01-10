a98c9acf0fccf1500ad0877fd3ce64bf2521bbfb
chrisxjacobi <chrisxjacobi@gmail.com>
2018-01-09 18:49:56 -0600

    tested maintenance middlewar with app.use

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,12 +1,31 @@
-// learned partials to reuse parts in site, running nodemon server.js -e,hbs (to watch handlebars files), used helpers to use for dynamic data
+// tested maintenance middlewar with app.use
 
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+if (err) {
+  console.log('Unable to append to server.log');
+}
+  })
+  next();
+})
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
