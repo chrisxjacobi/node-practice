c89e1615b401596255ee44170b6438389f1a95a5
chrisxjacobi <chrisxjacobi@gmail.com>
2018-01-14 13:42:41 -0600

    set up mongoose model validation and defaults, created a new model, installed postman...created db and models folders and exported them to server, added express and body-parser, had errors setting up server

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,41 +1,29 @@
-// installed mongoose and connected to database, set up to use Promises, created 2 new Todo models,
+// set up mongoose model validation and defaults, created a new model, installed postman...created db and models folders and exported them to server, added express and body-parser, had errors setting up server
 
-const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose.js')
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/User');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
-  }
+var app = express();
+
+app.use(bodyParser.json());
+
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo:', doc);
-// }, (e) => {
-//   console.log('Unable to save todo');
-// });
-
-
-var otherTodo = new Todo({
-  text: 'Drink whiskey',
-  completed: true,
-  completedAt: 6
-});
-
-otherTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unable to save todo', e);
-});
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+})
