99d9d385d5651f81db4b5a60161178237b08543f
chrisxjacobi <chrisxjacobi@gmail.com>
2018-01-24 12:43:20 -0600

    tested application push to herkou; does not work due to folder being in main app, but practiced mLab connections and tested in postman

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,4 +1,4 @@
-// set up port to deploy on heroku, set up start script and node engine in package.json, used mLab mongodb service to integrate mongodb with heroku app, configed mongodo_uri 
+// tested application push to herkou; does not work due to folder being in main app, but practiced mLab connections
 
 const express = require('express');
 const bodyParser = require('body-parser');
