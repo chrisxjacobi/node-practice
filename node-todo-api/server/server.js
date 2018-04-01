c438c38a4055a9f5da13fdb805e1eaa3b6a915d3
chrisxjacobi <chrisxjacobi@gmail.com>
2018-04-01 10:32:20 -0500

    added authentication for post and get routes using _creator property, getting access to user and token, setting _creator to id of logged in user, making them private

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -19,20 +19,24 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 // for creating a new todo
-app.post('/todos', (req, res) => {
-  var todo = new Todo({text: req.body.text});
+app.post('/todos', authenticate, (req, res) => {
+  var todo = new Todo({
+    text: req.body.text,
+    _creator: req.user._id
+  });
 
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
   });
-
 });
 
 // for fetching/reading todos
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos}); // send as object to later add other properties
   }, (e) => {
     res.status(400).send(e);
